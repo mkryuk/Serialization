4f998e97c213fd36619d050f3c40d6e1b5a56d58
mkryuk <mkryuk@gmail.com>
2014-12-01 15:15:26 +0200

    Radio added

---
+++ Serialization/Radio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    class Radio
+    {
+        public bool hasTweeters;
+        public bool hasSubWoofers;
+        public double[] stationPresets;
+        [NonSerialized]
+        public string radioId = "XF-552RR6";
+    }
+}
